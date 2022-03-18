26b2ad1930efbc352fe675658c704c03bc3db73b
Scorpio Gentles <ScorpioGentles@outlook.com>
2022-03-18 18:00:06 -0400

    Initial Commit

---
+++ Password_Generator/index.js
@@ -0,0 +1,4 @@
+let passwordOne = document.getElementById('passwordOne')
+let passwordTwo = document.getElementById('passwordTwo')
+let passwordThree = document.getElementById('passwordThree')
+let passwordFour = document.getElementById('passwordFour')
