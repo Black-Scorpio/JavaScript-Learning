2188f6e2b99b53bd565d705b72c415851e76a4fb
Scorpio Gentles <ScorpioGentles@outlook.com>
2022-03-19 09:22:24 -0400

    random password function created

--- Password_Generator/index.js
+++ Password_Generator/index.js
@@ -1,4 +1,19 @@
-let passwordOne = document.getElementById('passwordOne')
-let passwordTwo = document.getElementById('passwordTwo')
-let passwordThree = document.getElementById('passwordThree')
-let passwordFour = document.getElementById('passwordFour')
+// let passwordOne = document.getElementById('passwordOne')
+// let passwordTwo = document.getElementById('passwordTwo')
+// let passwordThree = document.getElementById('passwordThree')
+// let passwordFour = document.getElementById('passwordFour')
+
+
+
+function generateRandomPassword(length){
+  var charArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()~'
+  var passwordCreated = '';
+  for(var i = 0; i < length; i++){
+    passwordCreated += charArray.charAt(Math.floor(Math.random()*charArray.length));
+
+  }
+  return passwordCreated
+
+}
+
+console.log(generateRandomPassword(10));
